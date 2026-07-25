4845bdac796591a1fcad78076481a6a94c5f6418
James Bekis <bigjames@Mac.home>
2026-07-25 13:13:31 -0700

    - Adds assets for project images - Adds assets for social media images - Adds assets for profile image - Creates social profile files - Creates highlighted projects files - Creats css files - Adds new font files - Adds bootstrap

---
+++ src/SocialProfiles.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import SOCIAL_PROFILES from "./data/socialProfiles";
+
+class SocialProfile extends Component {
+    render() {
+
+        console.log('this.props', this.props);
+        const {link, image} = this.props.project;
+        
+        return (
+            <div style={{ display: 'inline-block' }}>
+                <a href={link} style={{ display: 'inline-block', width: 25, margin: 10}}>
+                    <img src={image} alt='Social media icon' style={{ width: '100%' }} />
+                </a>
+            </div>
+        )
+
+    }
+}
+
+class SocialProfiles extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Contact Me</h3>
+                <div>
+                    {
+                        SOCIAL_PROFILES.map(SOCIAL_PROFILE => {
+                            return (
+                                <SocialProfile key={SOCIAL_PROFILE.id} project={SOCIAL_PROFILE} />
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default SocialProfiles;
