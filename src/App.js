fcb25c8393bd727f70fcb1ed72094ae5df855ffe
James Bekis <bigjames@Mac.home>
2026-07-25 09:30:40 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor() {
+        super();
+        this.state = { displayBio: false };
+
+        this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
+    }
+
+    toggleDisplayBio() {
+        this.setState({ displayBio: !this.state.displayBio });
+    }
+
+    render () {
+
+        return (
+            <div>
+                <h1>Hello!</h1>
+                <p>My name is James Bekis. I'm a software engineer.</p>
+                <p>I'm always looking forward to working on meaningful projects.</p>
+                {
+                    this.state.displayBio ? ( 
+                        <div>
+                            <p>I live in Ontario, California and I code every day.</p>
+                            <p>My favorite language is JavaScript, and I think React.js is a lot of fun.</p>
+                            <p>Besides coding, I love to play golf. I currently hold a 23.2 ghin handicap.</p>
+                            <button onClick={this.toggleDisplayBio}>Read less</button>
+                        </div>
+                    ) : (
+                        <div>
+                            <button onClick={this.toggleDisplayBio}>Read more</button>
+                        </div>
+                    )
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
