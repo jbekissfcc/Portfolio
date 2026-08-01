3d0ab7f9d0303a753c17504527132c206c920a21
James bekis <james.bekis@gmail.com>
2026-08-01 16:42:19 -0700

    Section 5
    - React Router

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Projects from './Projects';
 import SocialProfiles from './SocialProfiles';
 import Title from './Title';
-import Jokes from './Jokes';
 import profile from 'url:../assets/jbekis_profile_headshot.jpeg';
 
 class App extends Component {
@@ -38,8 +37,6 @@ class App extends Component {
         }
         <hr />
         <Projects />
-        <hr />
-        <Jokes />
         <hr />
         <SocialProfiles />
       </div>
